dd7bcf8e7ada545f228f4fe3bb7a22b309df13bf
chabouni kamel <chabouni.kamel@courrier.uqam.ca>
2015-10-03 21:53:06 -0400

    Allo

--- src/testgit_01/TestGit_01.java
+++ src/testgit_01/TestGit_01.java
@@ -16,7 +16,7 @@ public class TestGit_01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Allo
     }
     
 }
